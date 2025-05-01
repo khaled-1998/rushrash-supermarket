a1f58bb94bb2bfb106aae5048afdc4547f77bcd7
khaled-1998 <76492352+khaled-1998@users.noreply.github.com>
2025-05-01 20:22:59 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <Head>
+        <title>سوبر ماركت رشراش</title>
+      </Head>
+      <h1>مرحبا بكم في سوبر ماركت رشراش</h1>
+      <p>ابدأ التسوق الآن!</p>
+    </div>
+  );
+}
