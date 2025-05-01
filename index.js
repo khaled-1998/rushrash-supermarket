eb0e747d78ddad733d43536d002d335c2d65b426
khaled-1998 <76492352+khaled-1998@users.noreply.github.com>
2025-05-01 20:30:35 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-
-import Head from 'next/head';
-
 export default function Home() {
-  return (
-    <div style={{ padding: 20 }}>
-      <Head>
-        <title>سوبر ماركت رشراش</title>
-      </Head>
-      <h1>مرحبا بكم في سوبر ماركت رشراش</h1>
-      <p>ابدأ التسوق الآن!</p>
-    </div>
-  );
+  return <h1>أهلاً بك في RushRash Supermarket</h1>;
 }
